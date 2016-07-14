c608c6f397d6a414b55aee3328421f2885acce5d
Luiz Paro <lg_paro@hotmail.com>
2016-07-14 16:46:23 -0300

    Creating a few utility methods in the class JDBCConnection.

--- src/main/java/br/com/devmedia/jdbc/connection/JDBCConnection.java
+++ src/main/java/br/com/devmedia/jdbc/connection/JDBCConnection.java
@@ -2,6 +2,8 @@ package br.com.devmedia.jdbc.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBCConnection {
@@ -15,6 +17,38 @@ public class JDBCConnection {
             Class.forName(JDBC_DRIVER);
             return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
         } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public static void close(Connection connection) {
+        try {
+            if(connection != null && connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public static void close(Connection connection, PreparedStatement statement) {
+        try {
+            if(statement != null && statement.isClosed()) {
+                statement.close();
+            }
+            close(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public static void close(Connection connection, PreparedStatement statement, ResultSet resultSet) {
+        try {
+            if(resultSet != null && resultSet.isClosed()) {
+                resultSet.close();
+            }
+            close(connection, statement);
+        } catch (SQLException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
