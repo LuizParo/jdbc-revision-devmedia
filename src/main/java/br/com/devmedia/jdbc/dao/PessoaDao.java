a6b4193561d60aa29f5e597793e47570baa906cd
Luiz Guilherme Paro <lg_paro@hotmail.com>
2016-07-15 23:13:59 -0300

    Creating methods to recover a List of people.

--- src/main/java/br/com/devmedia/jdbc/dao/PessoaDao.java
+++ src/main/java/br/com/devmedia/jdbc/dao/PessoaDao.java
@@ -5,6 +5,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.devmedia.jdbc.entity.Pessoa;
 
@@ -74,6 +76,63 @@ public class PessoaDao {
                 return pessoa;
             }
             throw new IllegalArgumentException("No records found for: " + nome);
+        } catch(SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public List<Pessoa> findAll() {
+        try {
+            List<Pessoa> people = new ArrayList<>();
+            StringBuilder sql = new StringBuilder("SELECT * FROM pessoa");
+            
+            PreparedStatement statement = this.connection.prepareStatement(sql.toString());
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                Pessoa pessoa = new Pessoa();
+                pessoa.setId(rs.getInt("id"));
+                pessoa.setNome(rs.getString("nome"));
+                pessoa.setProfissao(rs.getString("profissao"));
+                pessoa.setDataNascimento(rs.getDate("data_nascimento"));
+                
+                people.add(pessoa);
+            }
+            
+            return people;
+        } catch(SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public int removeAll() {
+        StringBuilder sql = new StringBuilder("DELETE FROM pessoa");
+        try {
+            return this.connection.createStatement().executeUpdate(sql.toString(), PreparedStatement.RETURN_GENERATED_KEYS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+    public List<Pessoa> findAllByProfissao(String profissao) {
+        try {
+            List<Pessoa> people = new ArrayList<>();
+            StringBuilder sql = new StringBuilder("SELECT * FROM pessoa WHERE profissao = ?");
+            
+            PreparedStatement statement = this.connection.prepareStatement(sql.toString());
+            statement.setString(1, profissao);
+            
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                Pessoa pessoa = new Pessoa();
+                pessoa.setId(rs.getInt("id"));
+                pessoa.setNome(rs.getString("nome"));
+                pessoa.setProfissao(rs.getString("profissao"));
+                pessoa.setDataNascimento(rs.getDate("data_nascimento"));
+                
+                people.add(pessoa);
+            }
+            
+            return people;
         } catch(SQLException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
--- src/test/java/br/com/devmedia/jdbc/dao/PessoaDaoTest.java
+++ src/test/java/br/com/devmedia/jdbc/dao/PessoaDaoTest.java
@@ -2,6 +2,7 @@ package br.com.devmedia.jdbc.dao;
 
 import java.sql.Connection;
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -23,17 +24,18 @@ public class PessoaDaoTest {
     
     @After
     public void tearDown() {
+        this.dao.removeAll();
         JDBCConnection.close(this.connection);
     }
     
-    private Pessoa createPerson() {
+    private Pessoa createPerson(String name) {
         Calendar dataNascimentoCalendar = Calendar.getInstance();
         dataNascimentoCalendar.set(Calendar.DAY_OF_MONTH, 2);
         dataNascimentoCalendar.set(Calendar.MONTH, Calendar.OCTOBER);
         dataNascimentoCalendar.set(Calendar.YEAR, 1985);
         
         Pessoa pessoa = new Pessoa();
-        pessoa.setNome("João da Silva");
+        pessoa.setNome(name);
         pessoa.setProfissao("Administrator");
         pessoa.setDataNascimento(dataNascimentoCalendar.getTime());
         return pessoa;
@@ -41,7 +43,7 @@ public class PessoaDaoTest {
 
     @Test
     public void shouldPersistPersonInDatabase() {
-        Pessoa pessoa = this.createPerson();
+        Pessoa pessoa = this.createPerson("João da Silva");
         
         int generatedId = this.dao.save(pessoa);
         Assert.assertTrue(generatedId > 0);
@@ -49,7 +51,7 @@ public class PessoaDaoTest {
     
     @Test
     public void shouldFindPersonById() {
-        Pessoa pessoa = this.createPerson();
+        Pessoa pessoa = this.createPerson("João da Silva");
         int generatedId = this.dao.save(pessoa);
         
         Pessoa pessoaRecuperada = this.dao.findById(generatedId);
@@ -58,10 +60,37 @@ public class PessoaDaoTest {
     
     @Test
     public void shouldFindPersonByNome() {
-        Pessoa pessoa = this.createPerson();
+        Pessoa pessoa = this.createPerson("João da Silva");
         this.dao.save(pessoa);
         
         Pessoa pessoaRecuperada = this.dao.findByNome(pessoa.getNome());
         Assert.assertEquals(pessoa.getNome(), pessoaRecuperada.getNome());
+    }
+    
+    @Test
+    public void shouldFindAllPeople() {
+        this.dao.save(this.createPerson("João da Silva"));
+        this.dao.save(this.createPerson("Maria da Silva"));
+        this.dao.save(this.createPerson("Fernando da Silva"));
+        
+        List<Pessoa> people = this.dao.findAll();
+        Assert.assertNotNull(people);
+        Assert.assertFalse(people.isEmpty());
+        Assert.assertEquals(3, people.size());
+    }
+    
+    @Test
+    public void shouldFindAllPeopleByProfissao() {
+        this.dao.save(this.createPerson("João da Silva"));
+        this.dao.save(this.createPerson("Maria da Silva"));
+        this.dao.save(this.createPerson("Fernando da Silva"));
+        
+        List<Pessoa> people = this.dao.findAllByProfissao("Administrator");
+        Assert.assertNotNull(people);
+        Assert.assertFalse(people.isEmpty());
+        Assert.assertEquals(3, people.size());
+        for (Pessoa person : people) {
+            Assert.assertEquals("Administrator", person.getProfissao());
+        }
     }
 }
